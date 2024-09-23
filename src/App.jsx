8c2125a330c48ec7093e951f76552815dc3fdfdb
mIzadyAmiri <mojtabaizady99@gmail.com>
2024-09-23 15:33:45 +0330

    nested routes + dynamic routes with url parameters

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,61 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import HomePage from './pages/HomePage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import City from './components/City';
+
+const BASE_URL = 'http://localhost:9000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
-        <Route path='app' element={<AppLayout />} />
+
+        <Route path='app' element={<AppLayout />}>
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route
+            path='cities'
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path='cities/:id' element={<City />} />
+          <Route
+            path='countries'
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path='form' element={<p>Form</p>} />
+        </Route>
+
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>

7a4ceba043e67c086c4fb219e2088e02feb2aa78
mIzadyAmiri <mojtabaizady99@gmail.com>
2024-09-24 11:15:55 +0330

    query string

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+      <button onClick={() => setSearchParams({ lat: 0, lng: 1 })}>Change position</button>
+    </div>
+  );
 }
 
 export default Map;

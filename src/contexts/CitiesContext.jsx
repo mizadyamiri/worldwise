d05348a3cd5774995c517a3373fb4c60ba38dc4d
mojtabaIzadyAmiri <mojtabaizady99@gmail.com>
2024-10-10 11:37:26 +0330

    delete city

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -60,6 +60,21 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      });
+
+      setCities(cities => cities.filter(city => city.id !== id));
+    } catch (err) {
+      alert(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContex.Provider
       value={{
@@ -68,6 +83,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
